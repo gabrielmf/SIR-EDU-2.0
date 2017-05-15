9db8279df608c7bd3d3ff1f4514e69876da2bb60
Gabriel Maschke <gabriel.maschke@gmail.com>
2017-05-14 21:44:36 -0300

    Changing styles

--- src/components/Slider/SliderItem.js
+++ src/components/Slider/SliderItem.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { RaisedButton } from 'material-ui'
+import { FlatButton } from 'material-ui'
 import './Slider.scss'
 
 const SliderItem = (props) => {
@@ -8,13 +8,13 @@ const SliderItem = (props) => {
     const url = 'http://localhost:3000/api/files/' + item._id;
     
     return (
-        <div class="slider-item thumbnail" style={{minWidth: 200, maxWidth: 250}}>
+        <div class="slider-item thumbnail">
             {type === 'image' ?  <img src={url}/> :
             <video draggable="true" preload="metadata" src={url}></video>}
             <p class="comment">{item.metadata.comment}</p>
             <div class="text-center">
-                <RaisedButton label="Visualizar"></RaisedButton>&nbsp;
-                { editable && <RaisedButton label="Inserir" primary={true}></RaisedButton>}
+                <FlatButton label="Visualizar"/>&nbsp;
+                { editable && <FlatButton label="Inserir" primary={true}/>}
             </div>
         </div>
     )
